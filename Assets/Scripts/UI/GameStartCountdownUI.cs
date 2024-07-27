f0774cfe8d59f21dfad2cc28d932c9879d19641d
Lucas-Tran <giothewind1@gmail.com>
2024-07-26 18:02:51 -0700

    Initial Commit

---
+++ Assets/Scripts/UI/GameStartCountdownUI.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameStartCountdownUI : MonoBehaviour {
+
+
+
+    private const string NUMBER_POPUP = "NumberPopup";
+
+
+    [SerializeField] private TextMeshProUGUI countdownText;
+
+
+    private Animator animator;
+    private int previousCountDownNumber;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
+
+        Hide();
+    }
+
+    private void KitchenGameManager_OnStateChanged(object sender, System.EventArgs e) {
+        if (KitchenGameManager.Instance.IsCountdownToStartActive()) {
+            Show();
+        } else {
+            Hide();
+        }
+    }
+
+    private void Update() {
+        int countDownNumber = Mathf.CeilToInt(KitchenGameManager.Instance.GetCountDownToStartTimer());
+        countdownText.text = countDownNumber.ToString();
+
+        if (previousCountDownNumber != countDownNumber) {
+            previousCountDownNumber = countDownNumber;
+            animator.SetTrigger(NUMBER_POPUP);
+            SoundManager.Instance.PlayCountdownSound();
+        }
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
+    }
+
+}
